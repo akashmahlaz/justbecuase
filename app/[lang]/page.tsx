22d4c779e8603787e33ea19a72e325e100bdde24
Akash <akash@justbecausenetwork.com>
2026-03-10 10:53:40 +0530

    new-commit

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,6 +4,7 @@ import { HeroSection } from "@/components/home/hero-section"
 import { GlobalSearchSection } from "@/components/home/global-search-section"
 import { HowItWorks } from "@/components/home/how-it-works"
 import { FeaturedProjects } from "@/components/home/featured-projects"
+import { FeaturedCandidates } from "@/components/home/featured-candidates"
 import { Testimonials } from "@/components/home/testimonials"
 import { CTASection } from "@/components/home/cta-section"
 
@@ -18,8 +19,9 @@ export default async function HomePage() {
       <main className="flex-1">
         <HeroSection />
         <GlobalSearchSection />
-        <FeaturedProjects />
         <HowItWorks />
+        <FeaturedProjects />
+        <FeaturedCandidates />
         <Testimonials />
         <CTASection />
       </main>
