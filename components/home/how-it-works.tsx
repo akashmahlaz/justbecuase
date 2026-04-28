e094e93a20a400d31a725ad60447dbb3b1facba2
akashmahlaz <akashdalla408@gmail.com>
2026-04-28 14:34:14 +0530

    akash commit

--- components/home/how-it-works.tsx
+++ components/home/how-it-works.tsx
@@ -86,7 +86,7 @@ export function HowItWorks() {
                 value="ngos" 
                 className="px-6 py-2 rounded-full text-sm font-semibold data-[state=active]:bg-background data-[state=active]:text-secondary data-[state=active]:shadow-sm"
               >
-                {home.forNGOsTab || "For NGOs"}
+                {home.forNGOsTab || "For NGOs / Social Enterprise"}
               </TabsTrigger>
             </TabsList>
           </div>
